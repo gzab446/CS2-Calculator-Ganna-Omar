e05ab83c26d8dcca3d965d678f89b9726dc0edb4
Ganna Zab <gzab@aucegypt.edu>
2025-04-01 17:27:25 +0200

    Added calculator.h header file

---
+++ calculator.h
@@ -0,0 +1,22 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+int add(int a, int b);
+int subtract(int a, int b);
+int multiply(int a, int b);
+double divide(int a, int b);
+
+// Function to calculate factorial
+long long factorial(int n);
+
+// Function to calculate GCD (Greatest Common Divisor)
+int gcd(int a, int b);
+
+// Function to calculate LCM (Least Common Multiple)
+int lcm(int a, int b);
+
+// Function to generate a random number within a given range
+int generateRandom(int min, int max);
+
+
+#endif
